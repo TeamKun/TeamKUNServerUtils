196c5c571207b4e6b0d7e97579d39d7e6804ab41
dai-1103 <bosatsunumnum@gmail.com>
2022-08-26 01:49:46 +0900

    fix:gw command

--- src/main/java/net/kunmc/lab/teamkunserverutils/command/GiveWorldeditPermission.java
+++ src/main/java/net/kunmc/lab/teamkunserverutils/command/GiveWorldeditPermission.java
@@ -1,7 +1,6 @@
 package net.kunmc.lab.teamkunserverutils.command;
 
 import java.util.List;
-import net.kunmc.lab.commandlib.CommandContext;
 import net.kunmc.lab.teamkunserverutils.State;
 import net.kunmc.lab.teamkunserverutils.common.constants.CommonConst;
 import net.kunmc.lab.teamkunserverutils.common.utils.MessageUtil;
@@ -9,37 +8,34 @@ import net.kunmc.lab.teamkunserverutils.common.utils.PluginUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 public class GiveWorldeditPermission extends BaseCommand {
 
   public GiveWorldeditPermission() {
     super(CommandName.GIVE_WORLDEDIT_PERMISSION);
     argument(argumentBuilder -> {
-      argumentBuilder.entityArgument("player");
+      argumentBuilder.entityArgument("player").execute((ctx) -> {
+        if (!PluginUtil.existsPlugin(CommonConst.LUCKPERMS)) {
+          ctx.sendFailure(
+              MessageUtil.getInfoMessage("エラー: このサーバーには" + CommonConst.LUCKPERMS + "が導入されていません"));
+          return;
+        }
+
+        List<Player> args = (List<Player>) ctx.getParsedArg("player");
+
+        if (args.size() == 0) {
+          ctx.sendFailure(
+              MessageUtil.getInfoMessage("エラー: 存在しないプレイヤーです"));
+          return;
+        }
+
+        CommandSender sender = ctx.getSender();
+        for (Player player : args) {
+          Bukkit.dispatchCommand(sender,
+              "lp user " + player.getName() + " parent add "
+                  + State.config.WEPermissionName.value());
+        }
+      });
     });
-  }
-
-  @Override
-  protected void execute(@NotNull CommandContext ctx) {
-    if (!PluginUtil.existsPlugin(CommonConst.LUCKPERMS)) {
-      ctx.sendFailure(
-          MessageUtil.getInfoMessage("エラー: このサーバーには" + CommonConst.LUCKPERMS + "が導入されていません"));
-      return;
-    }
-
-    List<Player> args = (List<Player>) ctx.getParsedArg("player");
-
-    if (args.size() == 0) {
-      ctx.sendFailure(
-          MessageUtil.getInfoMessage("エラー: 存在しないプレイヤーです"));
-      return;
-    }
-
-    CommandSender sender = ctx.getSender();
-    for (Player player : args) {
-      Bukkit.dispatchCommand(sender,
-          "lp user " + player.getName() + " parent add " + State.config.WEPermissionName.value());
-    }
   }
 }
